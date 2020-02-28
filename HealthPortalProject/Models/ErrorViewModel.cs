ea65f86ee9e2c566be279c8697b2bcd035307d37
Landon Thurmon <landonthurmon1@gmail.com>
2020-02-28 10:50:41 -0600

    Add project files.

---
+++ HealthPortalProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HealthPortalProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
